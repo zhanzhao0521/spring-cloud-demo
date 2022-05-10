b096c01d2d78f4d6b7759bf82d873e2898b86058
zhanzhao <17634727939@163.com>
2022-05-11 01:23:08 +0800

    cloud

---
+++ cloud-consumer-order/src/main/java/com/estrus/springcloud/OrderMain.java
@@ -0,0 +1,10 @@
+package com.estrus.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class OrderMain {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderMain.class,args);
+    }
+}
---
+++ cloud-provider-payment/src/main/java/com/estrus/springcloud/PaymentMain.java
@@ -0,0 +1,11 @@
+package com.estrus.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentMain {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain.class,args);
+    }
+}
