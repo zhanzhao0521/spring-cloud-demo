1d7677a8064b02d6109bf2da98aa9130f5072e37
zhanzhao <17634727939@163.com>
2022-05-11 11:47:35 +0800

    eureka集群

--- cloud-consumer-order/src/main/java/com/estrus/springcloud/OrderMain.java
+++ cloud-consumer-order/src/main/java/com/estrus/springcloud/OrderMain.java
@@ -2,7 +2,10 @@ package com.estrus.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
 @SpringBootApplication
+@EnableEurekaClient
 public class OrderMain {
     public static void main(String[] args) {
         SpringApplication.run(OrderMain.class,args);
--- cloud-eureka-server7002/src/main/java/com/estrus/springcloud/EurekaMain7002.java
+++ cloud-eureka-server7002/src/main/java/com/estrus/springcloud/EurekaMain7002.java
@@ -1,13 +1,14 @@
 package com.estrus.springcloud;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaClient
-public class PaymentMain {
+@EnableEurekaServer
+public class EurekaMain7002 {
     public static void main(String[] args) {
-        SpringApplication.run(PaymentMain.class,args);
+        SpringApplication.run(EurekaMain7002.class,args);
     }
 }
